6b60a32d3798e4a3b386904e14f36e930ebf8afe
Jasmine <bayramali5219@gmail.com>
2022-04-04 09:09:24 +0300

    Work 3 is done and about to be pushed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     //Doing some work here (you can type anything you want)
     //Doing awesome work2!
-    //
+    //Doing awesome work3!
+    //and ready to pushed to remote repository
 
 }
